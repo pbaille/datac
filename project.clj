bf10fe46ba769269cc1dffe931382736619219a0
pbaille <pierrebaille@gmail.com>
2016-01-11 19:25:40 +0100

    0.0.4 snapshot

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject datac "0.0.3"
+(defproject datac "0.0.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojars.gjahad/debug-repl "0.3.3"]
-                 [backtick "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 #_[org.clojars.gjahad/debug-repl "0.3.3"]
+                 [org.clojars.pbaille/utils "0.1.0-SNAPSHOT"]])
--- src/datac/core.clj
+++ src/datac/core.clj
@@ -1,381 +1,372 @@
 (ns datac.core
-  (:import [clojure.lang Counted Fn MapEntry IPersistentVector IPersistentList LazySeq IPersistentMap IPersistentSet Seqable Sequential])
-  (:use utils.futils)
-  (:require [alex-and-georges.debug-repl :as drepl]))
-
-(defmacro dr [] `(drepl/debug-repl))
+  (:import [clojure.lang Fn MapEntry IPersistentVector IPersistentList Iterate LongRange LazySeq IPersistentMap IPersistentSet Keyword Symbol Repeat Cycle Cons])
+  (:require [utils.futils :as fu]
+            [utils.types :refer [t t?]]
+            [utils.misc :refer [impl-for defmets]]
+            [clojure.test :refer :all]))
 
 (set! *print-length* 50)
 
-;; utils -------------
+;; helpers -----
 
-  (defn with-t [t x] (vary-meta x assoc :type t))
-  
-  (defn wtof [x y] (with-t (type x) y))
-  (defn wmof [x y] (with-meta y (meta x)))
+(defn mentry [k v] (MapEntry. k v))
 
-  (defmacro defmets [n & body]
-    `(do ~@(map (fn [spec] `(defmethod ~n ~@spec)) 
-                (partition 3 body))))
- 
-  (defn me [k v] (MapEntry. k v))
-  (defn me? [x] (instance? MapEntry x))
-  
-;; eseq --------------
-  
-  (defn eseq> [x] (with-t ::eseq x))
-  
-  (defn eseq! 
-    ([x] (get-in (meta x) [:behaviors :eseq]))
-    ([x f] (vary-meta x assoc-in [:behaviors :eseq] f)))
-  
-  (def-curried >eseq [x y] (eseq! y (eseq! x)))
-  
-  (defmulti eseq-m type)
-  
-  (defmets eseq-m
-    nil [x] () 
-    ::eseq [x] x
-    MapEntry [x] (list x)
-    IPersistentMap [x] (if-let [s (seq x)] s ())
-    IPersistentSet [x] (map me x x)
-    Sequential [x] (map-indexed me x)
-    :default [x] (list (me 0 x)))
-  
-  (defn eseq [x] 
-    (eseq> 
-      (if-let [e (:eseq (meta x))] 
-        (e x) 
-        (eseq-m x))))
-  
-  (defn eseq->hm [x]
-    (reduce #(assoc %1 (key %2) (val %2)) {} x))
-  
-;; build -------------
-  
-  (defn <>! 
-    ([x] (get-in (meta x) [:behaviors :<>]))
-    ([x f] (vary-meta x assoc-in [:behaviors :<>] f)))
-  
-  (def-curried ><> [x y] (<>! y (<>! x)))
+(defn eseq> [x & mtags]
+  (let [tagmap (apply hash-map (mapcat #(vector % true) mtags))]
+    (with-meta x (merge tagmap {:type ::eseq}))))
 
-  (defmulti <>m (comp type arg1))
+(defn- lazy? [eseq]
+  (:lazy (meta eseq)))
 
-  (defmets <>m
-    nil [x y] nil
-    ::eseq [x y] (wmof x (eseq y))
-    Fn [x y] (constantly y)
-    MapEntry [x y] (first (eseq y))
-    IPersistentVector [x y] (wmof x (mapv second (eseq y)))
-    IPersistentSet [x y] (wmof x (set (keep second (eseq y))))
-    IPersistentMap [x y] (wmof x (eseq->hm (eseq y)))
-    IPersistentList [x y] (wmof x (seq (map second (eseq y))))
-    LazySeq [x y] (wmof x (map second (eseq y)))
-    Sequential [x y] (wmof x (map second (eseq y)))
-    :default [x y] y)
+(defn eseq->hm [x]
+  (reduce #(assoc %1 (key %2) (val %2)) {} x))
 
-  (def-curried <> [x y]
-    (if-let [b (<>! x)] 
-      (b x y) 
-      (<>m x y)))
- 
-;; alter -------------
-  
-  (defn alt! 
-    ([x] (get-in (meta x) [:behaviors :alt]))
-    ([x f] (vary-meta x assoc-in [:behaviors :alt] f)))
-  
-  (def-curried >alt [x y] (alt! y (alt! x)))
-  
-  (defmulti altm (comp type arg1))
+(defn eseq->vec [x]
+  (reduce
+    (fn [r [k v]]
+      (if (integer? k)
+        (let [c (count r)]
+          (if (> c k)
+            (assoc r k v)
+            (conj (apply conj r (take (- k c) (repeat nil))) v)))
+        r))
+    []
+    x))
 
-  (defn alt [x f]
-    (if-let [a (alt! x)]
-      (a x f)
-      (altm x f)))
-    
-  (defmethod altm :default [x f] (f x))
-  
-;; invok -------------
+(declare §)
 
-  (declare &> $> *> §>)
+(defn zip-eseqs [x y]
+  ; is there a way to avoid this crap?
+  (let [lx (lazy? x)
+        ly (lazy? y)]
+    (cond
+      (and lx ly) (eseq> (map #(§ (val %1) %2) x y) :lazy)
+      ly (let [hx (into {} x)] (eseq> (map #(§ (find hx (key %)) %) y) :lazy))
+      lx (let [hy (into {} y)] (eseq> (map #(§ % (find hy (key %))) x) :lazy))
+      :else (eseq> (seq (merge-with § (into {} x) (into {} y)))))))
 
-  (defn §! 
-    ([x] (get-in (meta x) [:behaviors :§]))
-    ([x f] (vary-meta x assoc-in [:behaviors :§] f)))
-  
-  (def-curried >§ [x y] (§! y (§! x)))
+(defn juxt* [& args]
+  (fn [& xs] (mapv #(apply § % xs) args)))
 
-  (defmulti §m (comp type arg1))
+;; main impl -----
 
-  (defmets §m 
-    nil [x y] y
-    Fn [x y] (x y)
-    MapEntry [x y] (§> (val x) y)
-    Seqable [x y] ($> #(*> % y) x)
-    :default [x _] x)
+(defmulti behaviors type)
 
-  (def-curried §> [x y] 
-    (if-let [i (§! x)]
-      (alt y (partial i x))
-      (alt y (partial §m x))))
+(defn default-behaviors [x]
+  {:fn    (constantly x)
+   :zero  x
+   :val   x
+   :alt   (fn [f] (f x))
+   :eseq  (eseq> (list (mentry 0 x)))
+   :build (fn [y] (get (into {} y) 0))})
 
-;; dist --------------
-  
-  (defn $! 
-    ([x] (get-in (meta x) [:behaviors :$]))
-    ([x appkw] (vary-meta x assoc-in [:behaviors :$] appkw)))
-  
-  (def-curried >$ [x y] ($! y ($! x)))
+(defn instance-bs [x]
+  (when-let [gbs (:get-bs (meta x))] (gbs x)))
 
-  (defmulti $m (comp type arg2))
+(defn get-bs [x]
+  (merge (default-behaviors x) (behaviors x) (instance-bs x)))
 
-  (defmets $m 
-    nil [f x] nil
-    MapEntry [f x] (me (key x) (*> f (val x)))
-    Seqable [f x] (<> x ($> f (eseq x)))
-    ::eseq [f x] (wmof x (eseq> (map ($> f) x)))
-    :default [f x] (*> f x))
+(defn conj-op [x op]
+  (let [opx (:op (meta x))
+        op (if opx (conj opx op) [op])]
+    (vary-meta x assoc :op op)))
 
-  (prefer-method $m ::eseq Seqable)
+(defn clear-op [x]
+  (vary-meta x dissoc :op))
 
-  (def-curried $> [f x]
-    (if-let [m ($! x)]
-      (m f x)
-      ($m f x)))
+(declare c)
 
-;; zip ---------------
+(defn §
+  ([x] (conj-op x §))
+  ([x y]
+   (let [[bx by] (map get-bs [x y])
+         op (:op (meta x))]
+     (if op
+       ((apply c op) (clear-op x) y)
+       ((:alt by) (:fn bx))))))
 
-  (defn mape [f x]
-    (eseq> (map f (eseq x))))
-  
-  (defn &1! 
-    ([x] (get-in (meta x) [:behaviors :&1]))
-    ([x appkw] (vary-meta x assoc-in [:behaviors :&1] appkw)))
-  
-  (def-curried >&1 [x y] (&1! y (&1! x)))
-  
-  (defn &2! 
-    ([x] (get-in (meta x) [:behaviors :&2]))
-    ([x appkw] (vary-meta x assoc-in [:behaviors :&2] appkw)))
-  
-  (def-curried >&2 [x y] (&2! y (&2! x)))
-  
-  (defmulti &m (comp type arg1))
+(def f (fu/flip §))
 
-  (defmets &m 
-    IPersistentMap [x y] (<> y (mape #($> (get x (key %) nil) %) y))
-    Counted [x y] (&m (<> {} x) y)
-    LazySeq [x y] (<> y (mape #($> (nth x  (key %) nil) %) y))
-    :default [x y] (&m {0 x} y))
-  
-  (def-curried &> [x y]
-    (if-let [z (or (&2! y) (&1! x))] 
-      (z x y)
-      (if (coll? y)
-        (&m x y)
-        (val (first (&m x y))))))
-  
-;; plus --------------
+(defn $
+  ([x] (conj-op x $))
+  ([x y]
+   (let [[bx by] (map get-bs [x y])]
+     ((:build by) (map (partial § x) (:eseq by))))))
 
-  (defn +! 
-    ([x] (get-in (meta x) [:behaviors :+]))
-    ([x appkw] (vary-meta x assoc-in [:behaviors :+] appkw)))
-  
-  (def-curried >+ [x y] (+! y (+! x)))
-  
-  (defmulti +m (comp type arg2))
-  
-  (defmets +m 
-    Fn [x y] (if (fn? x) (juxt x y) [x y])
-    java.lang.Number [x y] (if (number? x) (+ x y) [x y])
-    String [x y] (if (string? x) (str x y) [x y])
-    Seqable [x y] (<> y (eseq> (concat (eseq x) (eseq y))))
-    :default [x y] [x y])
-  
-  (def-curried +> [x y]
-    (if-let [p (+! x)] 
-      (p x y)
-      (+m x y)))
+(defn &
+  ([x] (conj-op x &))
+  ([x y]
+   (let [[bx by] (map get-bs [x y])]
+     ((:build by) (zip-eseqs (:eseq bx) (:eseq by))))))
 
-;; zero --------------
-  
-  (defn o! 
-    ([x] (get-in (meta x) [:behaviors :o]))
-    ([x appkw] (vary-meta x assoc-in [:behaviors :o] appkw)))
-  
-  (def-curried >o [x y] (o! y (o! x)))
-  
-  (defmulti om type)
-  
-  (defmets om
-    Fn [x] identity
-    java.lang.Number [x] 0
-    String [x] ""
-    Seqable [x] (<> x [])
-    :default [x] x)
-  
-  (defn o> [x]
-    (if-let [zf (o! x)]
-      (zf x)
-      (om x)))
-  
-;; wild --------------
-  
-  (defn *! 
-    ([x] (get-in (meta x) [:behaviors :*]))
-    ([x appkw] (vary-meta x assoc-in [:behaviors :*] appkw)))
-  
-  (def-curried >* [x y] (*! y (*! x)))
+(fu/def-curried ◊ [x y]
+                (let [[bx by] (map get-bs [x y])]
+                  ((:build bx) (eseq> (concat (:eseq bx) (:eseq by))))))
 
-  (defmulti *m (comp type arg1))
-  
-  (defmethod *m :default [x y] (§> x y))
-  
-  (def-curried *> [x y] 
-    (if-let [*f (*! x)]
-      (*f x y)
-      (*m x y)))
+(defn o [x] (:zero (get-bs x)))
 
-;; behaviors sharing --
+;; behaviors set get share
 
-  (defn b! 
-    ([x] (:behaviors (meta x)))
-    ([x bm] (vary-meta x assoc :behaviors bm)))
+(defn b! [x bfn]
+  (vary-meta x assoc :get-bs bfn))
 
-  (def-curried >b [x y]
-    (vary-meta x assoc :behaviors (merge (b! x) (b! y))))
-  
-  (def-curried >b! [x y]
-    (vary-meta x assoc :behaviors (b! y)))
+(defn >b
+  ([x] (get-bs x))
+  ([x y]
+   (b! y
+       #(merge (default-behaviors %)
+               ((impl-for behaviors x) %)
+               (if-let [gbs (:get-bs (meta x))] (gbs %))))))
 
-;; flipped ------------
+;; extend native types ---
 
-  (def <§ (flip §>))
-  (def <* (flip *>))
-  (def <$ (flip $>))
-  (def <& (flip &>))
-  (def <+ (flip +>))
+(derive Repeat ::lazy)
+(derive Cycle ::lazy)
+(derive LazySeq ::lazy)
+(derive Cons ::lazy)
+(derive Iterate ::lazy)
+(derive LongRange ::lazy)
 
-;; wtf ----------------
+(defmets behaviors
 
-  (def-curried $$> [a b] ($> ($> $> a) b))
-  (def-curried $&> [a b] ($> ($> &> a) b))
-  (def-curried *$> [a b] (*> ($> $> a) b))
-  (def-curried *&> [a b] (*> ($> &> a) b))
-  (def-curried &$> [a b] (&> ($> $> a) b))
-  (def-curried &&> [a b] (&> ($> &> a) b))
+           nil [_]
+           {:fn    identity
+            :alt   (constantly nil)
+            :eseq  (eseq> ())
+            :build (constantly nil)}
 
-;; extras -------------
-  
-  ; (defn s> 
-  ;   "stack"
-  ;   [& xs]
-  ;   (with-meta xs 
-  ;    {:invok #(reduce <* %2 %1)
-  ;     :alt #(apply s> %2 %1)}))
-  
-  ; (defn r> 
-  ;   "reductions"
-  ;   [& xs]
-  ;   (with-meta xs 
-  ;    {:invok #(reductions <* %2 %1)
-  ;     :alt #(s> (partial invok %1) %2)}))
-  
-  ; (defn cr> 
-  ;   "cyclic reductions"
-  ;   [& xs]
-  ;   (with-meta xs 
-  ;    {:invok #(reductions <* %2 (cycle %1))
-  ;     :alt #(s> (partial invok %1) %2)}))
-  
-  ; (defn zm 
-  ;   "zipped hash-map"
-  ;   [& pairs]
-  ;   (& (apply hash-map pairs)))
-  
-  ; (defn nm 
-  ;   "nested hash-map
-  ;   (nm [:a :b] 1 :c 2) => {:c 2, :a {:b 1}}"
-  ;   [& pairs]
-  ;    (reduce 
-  ;     (fn [acc [cs v]] (assoc-in acc cs v))
-  ;     {}
-  ;     (map (fn [[a b]] [(if (vector? a) a [a]) b]) 
-  ;          (partition 2 pairs))))
-  
-  ; (defn- in-helper [op & pairs]
-  ;   (apply s>
-  ;     (map 
-  ;       (fn it [[[x & xs] v]] 
-  ;         (zm x (if (seq xs) (it [xs (op v)]) (op v))))
-  ;       (map (fn [[a b]] [(if (vector? a) a [a]) b]) 
-  ;            (partition 2 pairs)))))
-  
-  ; (defn *>> [x & pairs] (*> (apply in-helper *> pairs) x))
-  ; (defn f>> [x & pairs] (*> (apply in-helper f> pairs) x))
-  ; (defn $>> [x & pairs] (*> (apply in-helper $ pairs) x))
-  ; (defn &>> [x & pairs] (*> (apply in-helper & pairs) x))
-  
-  ; (defn squad 
-  ;   "group of elements that defer invocation and alteration to their leader
-  ;   (*> (squad [inc dec dec]) 1) => 2
-  ;   (*> inc (squad [0 1 2])) => 1
-  ;   (*> inc (squad last [12 2 3 8])) => 9"
-  ;   ([x] 
-  ;    (cond 
-  ;     (sequential? x) ((squad first) x)
-  ;     (fn? x)
-  ;     (fn [xs] 
-  ;      (with-meta xs
-  ;       {:alt #(*> %2 (x xs))
-  ;        :invok #(*> (x xs) %2)}))))
-  ;   ([lead-finder xs] ((squad lead-finder) xs)))
-  
-  ; (comment 
-  ;   (f>> {:a {:b 1} :c 2} [:a :b] inc :c dec)
-  ;   (*>> {:a {:b 1} :c 2} [:a :b] inc :c dec)
-  ;   ($>> {:a {:b [1 1]} :c [6 2]} [:a :b] inc :c dec)
-  ;   (&>> {:a {:b [1 1]} :c [6 2]} [:a :b] [inc dec] :c [dec inc])
-    
-  ;   (*> (squad [inc dec dec]) 1)
-  ;   (*> inc (squad [0 1 2]))
-  ;   (*> inc (squad last [12 2 3 8]))
-  ;   )
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+           ::eseq [x]
+           {:eseq  x
+            :build identity
+            :zero  (eseq> ())}
 
-  
-  
-  
-  
-  
-  
-  
-  
+           Fn [x]
+           {:zero identity
+            :fn   x}
+
+           Number [x] {:zero 0}
+           String [x] {:zero ""}
+           Keyword [x] {:zero (keyword "")}
+           Symbol [x] {:zero (symbol "")}
+
+           MapEntry [x]
+           {:fn    (fn [y] (mentry (key x) (§ (val x) y)))
+            :val   (val x)
+            :zero  (mentry (key x) nil)
+            :alt   (fn [f] (mentry (key x) (§ f (val x))))
+            :eseq  (eseq> (list x))
+            :build (fn [y] (find (into {} y) (key x)))}
+
+           IPersistentVector [x]
+           {:zero  []
+            :fn    (fn [y] ((apply juxt* x) y))
+            :eseq  (eseq> (map-indexed mentry x))
+            :build eseq->vec}
+
+           IPersistentMap [x]
+           {:zero  {}
+            :fn    (fn [y] (into {} (map #(§ % y) x)))
+            :eseq  (eseq> (map identity x))
+            :build eseq->hm}
+
+           IPersistentSet [x]
+           {:zero  #{}
+            :fn    (fn [y] (set ((apply juxt* x) y)))
+            :eseq  (eseq> (map-indexed mentry x))
+            :build #(set (map second %))}
+
+           IPersistentList [x]
+           {:zero  ()
+            :fn    (fn [y] (apply list ((apply juxt* x) y)))
+            :eseq  (eseq> (map-indexed mentry x))
+            :build #(apply list (map second %))}
+
+           ::lazy [x]
+           {:zero  (lazy-seq)
+            :fn    (fn [y] (map #(§ % y) x))
+            :eseq  (eseq> (map-indexed mentry x) :lazy)
+            :build #(map second %)}
+
+           :default [x] {})
+
+;; tests ------------------
+
+(testing "§"
+  (is (= (§ nil 1) 1))
+  (is (= (§ :pouet nil) nil))
+  (is (= (§ inc (mentry :a 1)) (mentry :a 2)))
+  (is (= (§ inc 1) 2))
+  (is (= (§ (mentry :a inc) 2) (mentry :a 3)))
+  (is (= (§ (mentry :a 1) 2) (mentry :a 1)))
+  (is (= (§ (mentry :a (mentry :b inc)) 2) (mentry :a (mentry :b 3))))
+  (is (= (§ 42 2) 42))
+  (is (= (§ 42 (mentry :a 1)) (mentry :a 42)))
+  (is (= (§ [inc dec] 1) [2 0]))
+  (is (= (§ [inc dec] (mentry :a 1)) (mentry :a [2 0])))
+  (is (= (§ {:a inc :b dec} 2) {:a 3, :b 1}))
+  (is (= (§ #{inc dec} 0) #{1 -1}))
+  (is (= (§ (list inc dec) 0) (list 1 -1)))
+  (is (= (§ (list [inc dec] [dec inc] {:a [[inc dec] dec] :b dec}) 1)
+         (list [2 0] [0 2] {:a [[2 0] 0], :b 0}))))
+
+(testing "$"
+  (is (= ($ nil 1) 1))
+  (is (= ($ :pouet nil) nil))
+  (is (= ($ 2 1) 2))
+  (is (= ($ 2 (mentry :a 1)) (mentry :a 2)))
+  (is (= ($ 2 inc) 2))
+  (is (= ($ inc 1) 2))
+  (is (= ($ inc (mentry :a 1)) (mentry :a 2)))
+  (is (= ($ (mentry :a inc) (mentry :a 1)) (mentry :a (mentry :a 2))))
+  (is (= ($ (mentry :a inc) 2) (mentry :a 3)))
+  (is (= ($ (mentry :a 1) inc) (mentry :a 1)))
+  (is (= ($ inc [0 1]) [1 2]))
+  (is (= ($ [inc dec] [0 1]) [[1 -1] [2 0]]))
+  (is (= ($ (mentry :a inc) [1 2])) [(mentry :a 2) (mentry :a 3)]))
+
+(testing "&"
+  (is (= (& :bob 2) :bob))
+  (is (= (& inc 2) 3))
+  (is (= (& 1 inc) 1))
+  (is (= (& nil :zux) :zux))
+  (is (= (& 1 [0 0 0]) [1 0 0]))
+  (is (= (& [inc dec] 1) 2))
+  (is (= (& partial inc) inc))
+  (is (= (& (mentry 2 inc) [0 0 0 0]) [0 0 1 0]))
+  (is (= (& [inc dec inc dec] [0 0 0 0]) [1 -1 1 -1]))
+  (is (= (& {:a inc :b dec} {:a 1 :b 1}) {:a 2, :b 0}))
+  (is (= (& {2 inc} [0 0 0 0]) [0 0 1 0]))
+  (is (= (& [[inc dec] [dec inc]] [1 2]) [[2 0] [1 3]]))
+  (is (= (take 10 (& (repeat inc) (range))) (take 10 (next (range)))))
+  (is (= (& {0 dec} #{1 2 3 4}) #{0 4 3 2})))
+
+(testing "◊"
+  (is (= (◊ 1 {0 2 :a 3}) 2))
+  (is (= (◊ nil 1) nil))
+  (is (= (◊ [] 1) [1]))
+  (is (= (◊ [1 2 3] (list 3 4)) [3 4 3]))
+  (is (= (◊ {} [:a :b :c]) {2 :c, 1 :b, 0 :a}))
+  (is (= (◊ [] {:a 1 2 0}) [nil nil 0]))
+  (is (= (◊ #{} [0 0 1]) #{0 1})))
+
+(testing "b!"
+  (let [x (b! [inc dec]
+              (fn [x]
+                {:fn  identity
+                 :alt (fn [f] (conj x f))}))]
+    [(§ dec x) (§ x 1) ($ 0 x)]))
+
+;; composition -----------
+
+(defn c [op & ops]
+  (fn [x y]
+    (if (seq ops)
+      (cond
+        (= op &) (& ($ #(partial (apply c ops) %) x) y)
+        (= op f) (§ (partial (apply c ops) y) x)
+        :else (op (partial (apply c ops) x) y))
+      (op x y))))
+
+(def f$ (c f $))
+(def f& (c f &))
+(def $$ (c $ $))
+(def && (c & &))
+(def &$ (c & $))
+(def $& (c $ &))
+
+(testing "c"
+  (is (= (f$ [1 2 3] inc)))
+  (is (= ($$ inc [[0 1] [2 3]])) [[1 2] [3 4]])
+  (is (= (&$ {:a inc :b dec} {:a [1 2] :b [2 3]}) {:b [1 2] :a [2 3]}))
+  (is (= (&& {:a [dec inc] :b [inc dec]} {:a [1 2] :b [2 3]}) {:a [0 3] :b [3 2]}))
+  (is (= ((c & & $) {:a [dec inc]} {:a [[1 2] [1 2]]}) {:a [[0 1] [2 3]]}))
+  (is (= ((c & $ $) {:a inc} {:a [[1 2] [3 4]]}) {:a [[2 3] [4 5]]}))
+  (is (= (take 10 (&$ (repeat inc) (repeat (range 10)))) (take 10 (repeat (next (range 11)))))))
+
+;; extras ----------------
+
+(defn !
+  "make a data always return itself when applied to anything"
+  [x]
+  (b! x (fn [x] {:fn (constantly x)})))
+
+(testing "!"
+  (is (= (§ (! [inc dec]) 1) [inc dec])))
+
+(defn s>
+  "stack"
+  [& xs]
+  (b! (t 'Stack xs)
+      (fn [x]
+        {:fn  #(reduce f % x)
+         :alt #(apply s> (conj (vec x) %))})))
+
+(testing "s>"
+  (is (= (§ (s> inc dec inc) 0)) 1)
+  (is (= (§ (§ (partial * 5) (s> inc dec inc)) 0) 5)))
+
+(defn r>
+  "reductions"
+  [& xs]
+  (b! (t 'Reductions xs)
+      (fn [x]
+        {:fn  #(reductions f % x)
+         :alt #(s> x %)})))
+
+(testing "r>"
+  (is (= (§ (r> inc inc inc) 0) (list 0 1 2 3)))
+  (is (= (§ (§ reverse (r> inc inc inc)) 0) (list 3 2 1 0))))
+
+(defn cr>
+  "cyclic reductions"
+  [& xs]
+  (b! (t 'CyclicReductions xs)
+      (fn [x]
+        {:fn  #(reductions f % (cycle x))
+         :alt #(s> x %)})))
+
+(testing "cr>"
+  (is (= (take 10 (§ (cr> inc dec) 0)) (take 10 (cycle [0 1]))))
+  (is (= (take 10 (§ (§ #(map inc %) (cr> inc dec)) 0)) (take 10 (cycle [1 2])))))
+
+(comment
+
+  "incubator experiments"
+
+  ;; args
+
+  (defn args* [& kvs]
+    (let [ks (take-nth 2 kvs)
+          find-key (into (apply hash-map (interleave ks ks)) (map-indexed vector ks))
+          v (apply sorted-map kvs)]
+      (b! v
+          (fn [x]
+            {:build (fn [y] (reduce #(assoc %1 (find-key (key %2)) (val %2)) y (filter (comp number? key) y)))}))))
+
+  (testing "args*"
+    #_(& [1 2] (args* :a 0 :b 0)))
+
+  ;; function type
+
+  (def ? '?)
+  (defn ?? [x] (when (= ? x) true))
+  (defn v [x] (:val (get-bs x)))
+
+  (defn dfn* [v fun]
+    (b! v
+        (fn [x]
+          {:build (fn [y] (dfn* (◊ v y) fun))
+           :val   (if (seq (keep (comp ?? val) x)) x (fun x))
+           :fn    (fn [y])})))
+
+  (defmacro dfn [argv & body]
+    (let [kws (mapv keyword argv)
+          v (into {} (map #(mentry % ?) kws))]
+      `(dfn* ~v (fn [x#] (apply (fn fun ~argv ~@body)
+                                (mapv (fn [y#] (y# x#)) ~kws))))))
+
+  (testing "dfn"
+    (is (= 4 (v (& {:a inc :b 2} (& {:a 1} (dfn [a b] (+ a b)))))))
+    ())
+
+  (filter (comp number? key) (eseq> {0 1 :a 1})))
